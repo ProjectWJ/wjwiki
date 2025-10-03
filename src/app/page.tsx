19792ba6e7ff9acb2fd2ff06cdaeffc8a22c3289
ProjectWJ <tlrhdxpa10@gmail.com>
2025-10-03 11:28:30 +0900

    @vercel/speed-insights 추가 및 페이지 컴포넌트에 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { useEffect, useState } from "react";
 import NavigationBar from "./navigationbar";
 import "../app/css/page.css";
@@ -107,6 +108,7 @@ export default function Home() {
         </div>
       </div>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
