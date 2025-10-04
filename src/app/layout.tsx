606ea5af2b39d5e2c380ad74d52740e13677222f
ProjectWJ <tlrhdxpa10@gmail.com>
2025-10-04 21:10:34 +0900

    react-markdown 패키지 설치, homebg.jpg 압축, charset utf-8 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
