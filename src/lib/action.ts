4b9e3ff050f79b9fd62c179a753dfc87872508cd
ProjectWJ <tlrhdxpa10@gmail.com>
2025-10-13 19:36:16 +0900

    미사용 미디어에 대한 자동 삭제 로직 cron cleanup 작업 테스트 완료(수동 실행해보니 정상동작)

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -215,9 +215,7 @@ export async function handleDeletePost(id: string): Promise<void> {
             
             // 🚨 일주일 후 삭제되도록 예약 시간을 설정합니다.
             const scheduledDeleteTime = new Date();
-            // scheduledDeleteTime.setDate(scheduledDeleteTime.getDate() + 7); // 7일 후
-            scheduledDeleteTime.setDate(scheduledDeleteTime.getDate() + 1); // 1일 후
-            // 위는 테스트용!!! 실제로는 7일 뒤로 설정하자.
+            scheduledDeleteTime.setDate(scheduledDeleteTime.getDate() + 7); // 7일 후
 
             await prisma.media.updateMany({
                 where: {
