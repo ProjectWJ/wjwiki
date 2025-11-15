0a0bf54cdfff384fd0d563a73406c596e6ab9e58
ProjectWJ <tlrhdxpa10@gmail.com>
2025-11-15 17:35:11 +0900

    dynamic import 설정

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -8,7 +8,6 @@ import { prisma } from '@/lib/db';
 import { revalidatePath } from 'next/cache'; // 데이터 갱신을 위해 필요
 import { extractFirstMediaUrl, findThumbnailUrl, ResizedImages, generateResizedImagesSharp, generateUUID, getFileExtension, howManyMedia } from '@/lib/server-utils' // 썸네일 생성
 import { vercelBlobUrl } from '@/constants/vercelblobURL';
-import DOMPurify from "isomorphic-dompurify";
 import * as cheerio from 'cheerio';
 
 const VIDEO_FORMATS = [
@@ -24,6 +23,15 @@ const VIDEO_FORMATS = [
     // 기존 코드에 있던 ".ts"를 포함하려면 여기에 추가해야 합니다.
 ];
 
+const getDOMPurify = async (htmlData: string) => {
+  // dynamic import() 사용
+  const { default: DOMPurify } = await import("isomorphic-dompurify");
+
+  // 불러온 DOMPurify 인스턴스를 사용하여 HTML 정제 (Sanitization)
+  const cleanHtml = DOMPurify.sanitize(htmlData);
+  
+  return cleanHtml;
+};
 
 // 게시물 생성 폼 제출을 처리하는 서버 액션
 // @param formData 폼 데이터를 포함하는 객체
@@ -32,7 +40,7 @@ export async function handleCreatePost(formData: FormData) {
   const title = formData.get('title') as string;
   const category_select = formData.get('category_select') as string || "diary";
   const rawContent = formData.get('content') as string;
-  const content = DOMPurify.sanitize(rawContent); // xss 정화
+  const content = await getDOMPurify(rawContent); // xss 정화
   const is_published = formData.get('is_published') === 'on' ? false : true; // 체크박스가 off일 때 true
   const summary = cheerio.load(content).text().trim().substring(0, 50); // 요약은 내용의 앞 50자로 자동 생성
   const firstMedia = extractFirstMediaUrl(content); // 첫 번째 미디어
@@ -116,7 +124,7 @@ export async function handleUpdatePost(formData: FormData): Promise<void> {
     const category_select = formData.get('category_select') as string || "diary";
     const legacyContent = formData.get("legacy_content") as string;
     const rawContent = formData.get('content') as string;
-    const content = DOMPurify.sanitize(rawContent); // xss 정화
+    const content = await getDOMPurify(rawContent); // xss 정화
     const legacyIs_published = formData.get('legacy_is_published') === 'on' ? false : true;
     const is_published = formData.get('is_published') === 'on' ? false : true; // 체크박스가 off일 때 true
 /*     const summary = content.substring(0, 50); // 요약은 내용의 앞 50자로 자동 생성
